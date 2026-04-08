104ef3f03f351e35b5a9986ee211c59557b20e89
excotide <labiqjazz@gmail.com>
2026-04-08 22:06:23 +0700

    new evolution

--- routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::view('/', 'pages.landing')->name('landing');
-Route::post('/contact', [ContactController::class, 'send'])->name('contact.send');
